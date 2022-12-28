c0e794d1d0af6d7f59477d7568de2920454eab1a
TEJROOP <btejroop77@gmail.com>
2022-12-28 20:51:19 +0530

    Initial Commit

---
+++ Restart/Screens/ContentView.swift
@@ -0,0 +1,28 @@
+//
+//  ContentView.swift
+//  Restart
+//
+//  Created by TEJROOP BONAGIRI on 28/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
+    var body: some View {
+        ZStack{
+            if isOnboardingViewActive{
+                OnboardingView()
+            }else{
+                HomeView()
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .previewDevice("iPhone 13 Pro")
+    }
+}
